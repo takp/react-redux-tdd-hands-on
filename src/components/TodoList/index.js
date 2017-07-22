135f3aafa58718fb9b5d22640bca95b1c9b6571d
Takayoshi Nishida <takayoshi.nishida@gmail.com>
2017-07-22 17:32:17 +0900

    Make TodoList to be a Class

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-/*
 class TodoList extends Component {
-}
- */
+    componentDidMount() {
+        this.props.actions.fetchTodos()
+    }
 
-const TodoList = ({todos}) => (
-    <ul className="todo-list">
-        { todos.map(todo => <li key="{todo.id}">{todo.title}</li>) }
-    </ul>
-)
+    render() {
+        const { todos } = this.props
+
+        return (
+            <ul className="todo-list">
+                { todos.map(todo => <li key={todo.id}>{todo.title}</li>) }
+            </ul>
+        )
+    }
+}
 
 export default TodoList
--- src/components/__tests__/TodoList.spec.js
+++ src/components/__tests__/TodoList.spec.js
@@ -2,7 +2,7 @@
 
 import TodoList from '../TodoList'
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 describe('TodoList', () => {
     const todos = [
@@ -15,15 +15,25 @@ describe('TodoList', () => {
             title: 'todo2'
         }
     ]
-    const wrapper = shallow(<TodoList todos={todos} />)
+
+    const actions = {
+        fetchTodos: jest.fn()
+    }
+
+    const wrapper = mount(<TodoList todos={todos} actions={actions} />)
 
     it("has a 'ul.todo-list'", () => {
         expect(wrapper.find('.todo-list')).toHaveLength(1)
     })
+
     it("has two todos", () => {
         const expected_text = wrapper.find('li').map(node => {
             return node.text()
         })
         expect(['todo1', 'todo2']).toEqual(expected_text)
+    })
+
+    it("should call once fetchTodos", () => {
+        expect(actions.fetchTodos.mock.calls).toHaveLength(1)
     })
 })
