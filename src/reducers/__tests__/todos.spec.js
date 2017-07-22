c7c85a50129bdb31bf7aa4a450348216942f881e
Takayoshi Nishida <takayoshi.nishida@gmail.com>
2017-07-22 16:36:31 +0900

    Make reducer to correspond to FETCH_TODOS_SUCCESS

--- src/reducers/__tests__/todos.spec.js
+++ src/reducers/__tests__/todos.spec.js
@@ -4,4 +4,12 @@ describe('todos reducer', () => {
     it('returns initial state', () => {
         expect(reducer(undefined, {})).toEqual([])
     })
+    
+    it('returns todos', () => {
+        const todos = {
+            id: 1,
+            title: 'todo1'
+        }
+        expect(reducer([], {type: 'FETCH_TODOS_SUCCESS', todos})).toEqual(todos)
+    })
 })
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -2,6 +2,8 @@ const initialState = []
 
 const todos = (state = initialState, action) => {
     switch(action.type) {
+        case 'FETCH_TODOS_SUCCESS':
+            return action.todos
         default:
             return state
     }
